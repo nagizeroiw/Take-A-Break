625c16da9debefe82c8ae495a34eab7495f52a84
nagizero <nagizero@foxmail.com>
2015-08-25 09:49:44 +0800

    first commit

---
+++ qttest/main.cpp
@@ -0,0 +1,24 @@
+﻿#include "qttest.h"
+#include <QtWidgets/QApplication>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QPixmap* pm = new QPixmap(32, 32);
+    // pm->fill(QColor(200, 200, 100));
+
+    QIcon* icon = new QIcon(":/qttest/Resources/copy.png");
+
+    MyTray* tray = new MyTray();
+
+    tray->setIcon(*icon);
+
+    // qDebug(QDir::currentPath().toStdString().c_str());
+
+    tray->start();
+    // tray->timeoutMessage();
+
+    return a.exec();
+}
---
+++ qttest/qttest.h
@@ -0,0 +1,127 @@
+#ifndef QTTEST_H
+#define QTTEST_H
+
+#include <QApplication>
+
+#include <QSystemTrayIcon>
+#include <QIcon>
+#include <QPixmap>
+#include <QDir>
+#include <Qtimer>
+#include <QObject>
+#include <QMenu>
+#include <QKeySequence>
+
+// whatever, just let memory leak
+class MyTray : public QSystemTrayIcon
+{
+    Q_OBJECT
+
+public:
+    MyTray() {
+
+        // break time
+        breaktime_msec = 1000 * 60 * 30; // half an hour
+
+        // build timer
+        timer = new QTimer();
+
+
+        // tip
+        QString tooltip("Take A Break!");
+        this->setToolTip(tooltip);
+
+        // contet menu
+        menu = new QMenu();
+        // quit
+        action_quit = new QAction(menu);
+        action_quit->setText("Quit      ");
+        action_quit->setShortcut(QKeySequence(tr("Ctrl+Q")));
+        menu->addAction(action_quit);
+        connect(action_quit, SIGNAL(triggered(bool)), qApp, SLOT(quit()));
+
+        menu->addSeparator();
+
+        // revive
+        action_revive = new QAction(menu);
+        action_revive->setText("Revive    ");
+        action_revive->setShortcut(QKeySequence(tr("Ctrl+W")));
+        menu->addAction(action_revive);
+        connect(action_revive, SIGNAL(triggered(bool)), this, SLOT(reviveTimer()));
+        // stop
+
+        menu->addSeparator();
+
+        action_stop = new QAction(menu);
+        action_stop->setText("Stop      ");
+        action_stop->setShortcut(QKeySequence(tr("Ctrl+T")));
+        menu->addAction(action_stop);
+        connect(action_stop, SIGNAL(triggered(bool)), this, SLOT(stopTimer()));
+        // start
+        action_start = new QAction(menu);
+        action_start->setText("Start     ");
+        action_start->setShortcut(QKeySequence(tr("Ctrl+O")));
+        action_start->setDisabled(true);
+        menu->addAction(action_start);
+        connect(action_start, SIGNAL(triggered(bool)), this, SLOT(startTimer()));
+        // add menu
+        this->setContextMenu(menu);
+    }
+    void start() {
+        this->show();
+        
+
+        QObject::connect(timer, SIGNAL(timeout()), 
+                         this,  SLOT(timeoutMessage()));
+
+        timer->start(breaktime_msec); // half an hour
+
+        this->showMessage("Timer started!",
+                          QString("Timer is started.\n"
+                          "From now on, you will be reminded "
+                          "to take a break for every 30 minutes."));
+    }
+
+public slots:
+    void timeoutMessage() {
+        this->showMessage("Take a break!", 
+                          QString("You have beening working for too long.\n"
+                          "Stand up and take a brief break!"));
+    }
+    void stopTimer() {
+        timer->stop();
+        this->showMessage("Timer stoped!", 
+                          QString("Timer has been stoped.\n"
+                          "Please do not to open it when came back."));
+        action_stop->setDisabled(true);
+        action_start->setEnabled(true);
+    }
+    void startTimer() {
+        timer->start(breaktime_msec);
+        this->showMessage("Timer started!", 
+                          QString("Timer has been started.\n"
+                          "Next reminder comes in 30 minutes."));
+        action_start->setDisabled(true);
+        action_stop->setEnabled(true);
+    }
+    void reviveTimer() {
+        timer->stop();
+        timer->start(breaktime_msec);
+        this->showMessage("Timer revived!", 
+                          QString("Timer has been cleared.\n"
+                          "Next reminder comes in 30 minutes."));
+    }
+private:
+
+    QMenu* menu;
+    QAction* action_revive;
+    QAction* action_stop;
+    QAction* action_start;
+    QAction* action_quit;
+    QTimer* timer;
+    int breaktime_msec;
+
+    MyTray(const MyTray& t);
+    MyTray& operator=(const MyTray& t);
+};
+#endif // QTTEST_H
